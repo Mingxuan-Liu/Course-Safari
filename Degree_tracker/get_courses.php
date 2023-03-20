2f7695dadf76baa3e9027e8f3bf17f95774a8afc
Mingxuan Liu <mingxuan.liu@emory.edu>
2023-03-20 12:43:25 -0400

    Changes to course_progress
    Right now I could only show courses that are already taken. But I also plan to show courses than needs to be taken in the future with different colors.

--- Degree_tracker/get_courses.php
+++ Degree_tracker/get_courses.php
@@ -14,20 +14,29 @@ $user_id = $_SESSION["user_id"];
 if (isset($_GET["major"])) {
     $clickedName = $_GET["major"];
 
+    echo "<div class='courses-container'>"; // Start the courses container
+    // Retrieve major courses that have not been taken yet
+
     // Retrieve course information based on the clicked major
     $sql = "SELECT * FROM courses_taken
     WHERE user_id = '$user_id' AND major_name = '$clickedName'
     ORDER BY course_num";
-    $result = mysqli_query($conn, $sql);
-
-    while ($obj = mysqli_fetch_assoc($result)) {
-        $temp_str = $obj['course_prefix'] . " " . $obj['course_num'] . ": " . $obj['course_name'];
+    $taken = mysqli_query($conn, $sql);
+    
+    while ($obj = mysqli_fetch_assoc($taken)) {
+        $taken_str = $obj['course_prefix'] . " " . $obj['course_num'] . ": " . $obj['course_name'];
+        echo "<div class='taken-block'>";
+        echo "<h4>" . $taken_str . "</h4>";
+        $course_tag = "";
         if ($obj['required'] == 1) {
-            $temp_str = $temp_str . " (required)";
+            $course_tag = "Required";
+            echo "<span class='require-tag'>" . $course_tag . "</span>";
         } else {
-            $temp_str = $temp_str . " (elective)";
+            $course_tag = "Elective";
+            echo "<span class='elective-tag'>" . $course_tag . "</span>";
         }
-        echo "<h4>" . $temp_str . "</h4>";
+        echo "</div>";
     }
+    echo "</div>"; // Close the courses container
 }
 ?>
