2176c77eb22def6240e34fa880b189001f4b76dd
TianjunZhong <tianjun.zhong@emory.edu>
2023-03-28 12:44:18 -0400

    Email Confirmation
    The gmail smtp for email confirmation is set up. The password resetting function is implemented.

---
+++ Reset_Password/reset-password.php
@@ -0,0 +1,155 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION["error"])) {
+        unset($_SESSION["error"]);
+    }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "user_db";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    if ($_GET["key"] && $_GET["reset"]) {
+        $_SESSION["email"] = $_GET["key"];
+        $_SESSION["password"] = $_GET["reset"];
+    }
+    elseif (!isset($_SESSION["email"]) || !isset($_SESSION["password"])) {
+        $_SESSION["error"] = "key_not_passed";
+    }
+    elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $newPassword = $_POST["newPassword"];
+        $confirmPassword = $_POST["confirmPassword"];
+        $email = $_SESSION["email"];
+        $oldPassword = $_SESSION["password"];
+
+        $check_sql = "SELECT * FROM users WHERE email = '$email' AND password = '$oldPassword'";
+        $check_result = mysqli_query($conn, $check_sql);
+        $user = mysqli_fetch_assoc($check_result);
+
+        if (mysqli_num_rows($check_result) < 1) {
+            $_SESSION["error"] = "link_expire";
+        }
+        elseif (password_verify($newPassword, $oldPassword)) {
+            $_SESSION["error"] = "same_password";
+        }
+        elseif ($newPassword != $confirmPassword) {
+            $_SESSION["error"] = "password_mismatch";
+        }
+        else {
+            $newPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+            $sql = "UPDATE users SET password = '$newPassword' WHERE email = '$email' AND password = '$oldPassword'";
+
+            if (mysqli_query($conn, $sql)) {
+                $_SESSION["reset_success"] = true;
+            } else {
+                $_SESSION["error"] = "failed";
+            }
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Tianjun Zhong">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="This page allows users to update their Course Safari usernames.">
+    <title>Update Username</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <div class = "container">
+        <div class="main">
+
+            <?php
+                if (isset($_SESSION["error"])): 
+            ?>
+                <section class="error-message">
+                    <i class="fas fa-exclamation-circle"></i>
+
+                    <?php if ($_SESSION["error"] == "key_not_passed"): ?>
+                        <h1>An Error Occured</h1>
+                        <p>An Error Occured. Please request another link.</p>
+                        <a href = "email.html" class = "buttons">Request Another Link</a>
+
+                    <?php elseif ($_SESSION["error"] == "link_expire"): ?>
+                        <h1>An Error Occured</h1>
+                        <p>Your are probably using a wrong or expired link. Please request another link.</p>
+                        <a href = "email.html" class = "buttons">Request Another Link</a>
+
+                    <?php elseif ($_SESSION["error"] == "same_password"): ?>
+                        <h1>Reset Failed</h1>
+                        <p>New password cannot be the same as the current password. Please use another password.</p>
+                        <a href = "reset-password.php" class = "buttons">Try Again</a>
+
+                    <?php elseif ($_SESSION["error"] == "password_mismatch"): ?>
+                        <h1>Reset Failed</h1>
+                        <p>New password mismatch. Please confirm you new password again.</p>
+                        <a href = "reset-password.php" class = "buttons">Try Again</a>
+
+                    <?php elseif ($_SESSION["error"] == "failed"): ?>
+                        <h1> Reset Failed</h1>
+                        <p>An error occured during resetting. Please try again later.</p>
+                        <a href = "reset-password.php" class = "buttons">Try Again</a>
+
+                    <?php endif; ?>
+
+                </section>
+
+            <?php 
+                elseif (isset($_SESSION["reset_success"])):
+            ?>
+
+                <section class="success-message">
+                    <h1>Success!</h1>
+
+                    <p>Password Reset Successful!</p>
+                    <p>Please use the new password to lgoin.<p>
+
+                    <a href = "../Login_Register/welcome.php" class = "buttons">Login</a>
+                </section>
+
+            <?php 
+                else:
+            ?>
+
+                <section>
+
+                    <form action="reset-password.php" , method="post">
+                        <p>
+                            <label for="newPassword">New Password: </label>
+                            <input type="password" name="newPassword" id="newPassword" placeholder="Enter your new password" required>
+                        </p>
+                        <p>
+                            <label for="confirmPassword">Confirm New Password: </label>
+                            <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirm your new password" required>
+                        </p>
+                        <br>
+                        <button type="submit">Update</button>
+                        <button type="reset">Clear</button>
+                    </form>
+
+                </section>
+
+            <?php 
+                endif;
+                unset($_SESSION["reset_success"]);
+            ?>
+
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Reset_Password/send-link.php
@@ -0,0 +1,140 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require '../PHPMailer/src/Exception.php';
+    require '../PHPMailer/src/PHPMailer.php';
+    require '../PHPMailer/src/SMTP.php';
+
+    session_start();
+
+    if (isset($_SESSION["error"])) {
+        unset($_SESSION["error"]);
+    }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "user_db";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$conn) {
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+      
+        $check_sql = "SELECT * FROM users WHERE email = '$email'";
+        $check_result = mysqli_query($conn, $check_sql);
+        $user = mysqli_fetch_assoc($check_result);
+
+        if (mysqli_num_rows($check_result) < 1) {
+            $_SESSION["error"] = "not_found";
+        }
+
+        else {
+            $password = $user["password"];
+            $link = "localhost/Course-Safari/Reset_Password/reset-password.php?key=".$email."&reset=".$password;
+            $username = $user["username"];
+            $body = "
+            <p>Hi $username,</p>
+            <p>Please use the below link to reset your password.</p>
+            <br>
+            <p>$link</p>
+            <br>
+            <p>Best,</p>
+            <p>Course Safari Team</p>";
+
+
+            $mail = new PHPMailer(true);
+
+            try {
+                $mail->isSMTP();                       
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+
+                $mail->Username = 'coursesafari@gmail.com';
+                $mail->Password = 'yrxhczrxvmclhwsg';
+
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+                $mail->Port = 465;
+
+                $mail->setFrom('coursesafari@gmail.com', 'Course Safari');
+                $mail->addAddress($email);
+
+                $mail->isHTML(true);
+                $mail->Subject = 'Reset Password';
+                $mail->Body = $body;
+
+                $mail->send();
+            } catch (Exception $e) {
+                $_SESSION["error"] = "not_sent";
+            }
+        }
+    }
+    else {
+        echo "Error: request not received, please try again.";
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Tianjun Zhong">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="This page allows users to update their Course Safari usernames.">
+    <title>Update Username</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+    <div class = "container">
+        <div class="main">
+
+            <?php
+                if (isset($_SESSION["error"])): 
+            ?>
+                <section class="error-message">
+                    <i class="fas fa-exclamation-circle"></i>
+                    <h1>An Error Occured</h1>
+
+                    <?php if ($_SESSION["error"] == "not_found"): ?>
+                        <p>Account registered with <?php echo $email?> is not found. Please check your email address.</p>
+                    <?php elseif ($_SESSION["error"] == "not_sent"): ?>
+                        <p>An error occured. The reset link wasn't sent successfully. Please try again.</p>
+                    <?php else: ?>
+                        <p>An error occured. Please try again later.</p>
+                    <?php endif; ?>
+
+                    <a href = "email.html" class = "buttons">Try Again</a>
+                </section>
+
+            <?php 
+                else: 
+            ?>
+
+                <section class="success-message">
+                    <h1>Success!</h1>
+
+                    <p>The reset link was sent to your through email successfully!</p>
+                    <p>Please use the link to reset your password.<p>
+
+                    <a href = "../Login_Register/welcome.php" class = "buttons">Login</a>
+                </section>
+
+            <?php 
+                endif;
+            ?>
+
+        </div>
+    </div>
+</body>
+
+</html>
