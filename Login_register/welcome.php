153b376a233e0e21d0359e202cd9d36519fc2d4f
radioactive235 <nicknianji@gmail.com>
2023-04-11 00:53:21 -0400

    background image

--- Login_register/welcome.php
+++ Login_register/welcome.php
@@ -4,10 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-  <link rel="stylesheet" href="login_register_style.css">
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"> -->
+  <link rel="stylesheet" href="welcome_style.css">
   <title>Welcome to Course Safari</title>
 </head>
+
 <body>
   <div class="container">
     <div class="main">
@@ -17,6 +18,12 @@
     <div class="buttons-container">
       <a href="login.php" class="btn btn-primary">Login</a>
       <a href="register.php" class="btn btn-secondary">Register</a>
+    </div>
+  </div>
+
+  <div class="image">
+    <div class="image-overlay">
+        
     </div>
   </div>
 </body>
